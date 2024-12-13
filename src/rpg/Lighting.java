d26d7b0b1826c9f16bd5eae5df5022b5c8549cfe
TranHa161 <tranha16105@gmail.com>
2024-12-14 03:17:21 +0700

    UpdateLighting

--- src/rpg/Lighting.java
+++ src/rpg/Lighting.java
@@ -13,42 +13,48 @@ public class Lighting {
 	
 	public Lighting(Framework fw, int circleSize) {
 		//Create a buffered image
-		darknessFilter = new BufferedImage(2000, 1124, BufferedImage.TYPE_INT_ARGB);
+		darknessFilter = new BufferedImage(1920, 1080, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = (Graphics2D)darknessFilter.getGraphics();
 		
 		//Get the center x và y of the light circle
-		int centerX = 2000/2;
-		int centerY = 1124/2;
+		int centerX = 1920/2;
+		int centerY = 1080/2;
 		
 		//Create a gradation effect
-		Color color[] = new Color[12];
-		float fraction[] = new float[12];
+		Color color[] = new Color[15];
+		float fraction[] = new float[15];
 		
-		color[0] = new Color(0,0,0,0.94f);
-		color[1] = new Color(0,0,0,0.93f);
-		color[2] = new Color(0,0,0,0.90f);
-		color[3] = new Color(0,0,0,0.85f);
-		color[4] = new Color(0,0,0,0.82f);
-		color[5] = new Color(0,0,0,0.82f);
-		color[6] = new Color(0,0,0,0.83f);
-		color[7] = new Color(0,0,0,0.85f);
-		color[8] = new Color(0,0,0,0.90f);
-		color[9] = new Color(0,0,0,0.91f);
-		color[10] = new Color(0,0,0,0.88f);
-		color[11] = new Color(0,0,0,0.85f);
+		color[0] = new Color(0,0,0,0.82f);
+		color[1] = new Color(0,0,0,0.76f);
+		color[2] = new Color(0,0,0,0.72f);
+		color[3] = new Color(0,0,0,0.65f);
+		color[4] = new Color(0,0,0,0.60f);
+		color[5] = new Color(0,0,0,0.54f);
+		color[6] = new Color(0,0,0,0.45f);
+		color[7] = new Color(0,0,0,0.35f);
+		color[8] = new Color(0,0,0,0.37f);
+		color[9] = new Color(0,0,0,0.45f);
+		color[10] = new Color(0,0,0,0.53f);
+		color[11] = new Color(0,0,0,0.58f);
+		color[12] = new Color(0,0,0,0.60f);
+		color[13] = new Color(0,0,0,0.60f);
+		color[14] = new Color(0,0,0,0.56f);
 		
 		fraction[0] = 0f;
-		fraction[1] = 0.4f;
-		fraction[2] = 0.5f;
-		fraction[3] = 0.6f;
-		fraction[4] = 0.65f;
-		fraction[5] = 0.7f;
-		fraction[6] = 0.75f;
-		fraction[7] = 0.8f;
-		fraction[8] = 0.85f;
-		fraction[9] = 0.9f;
-		fraction[10] = 0.95f;
-		fraction[11] = 1f;
+		fraction[1] = 0.2f;
+		fraction[2] = 0.3f;
+		fraction[3] = 0.4f;
+		fraction[4] = 0.5f;
+		fraction[5] = 0.55f;
+		fraction[6] = 0.6f;
+		fraction[7] = 0.65f;
+		fraction[8] = 0.7f;
+		fraction[9] = 0.75f;
+		fraction[10] = 0.8f;
+		fraction[11] = 0.85f;
+		fraction[12] = 0.9f;
+		fraction[13] = 0.95f;
+		fraction[14] = 1f;
 		
 		// Create a gradation paint settings
 		RadialGradientPaint gPaint = new RadialGradientPaint(centerX, centerY, (circleSize/2), fraction, color);
